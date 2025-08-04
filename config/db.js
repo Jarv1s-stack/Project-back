54c39f5ce990227ff257416d5dbb978a5214d65e
Jarv1s-stack <ayanabdimutalip@gmail.com>
2025-08-04 16:18:59 +0500

    change file db.js for link render dashboard

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'eventmate',
-    password: '1234',
-    port: 5432,
+    connectionString: 'postgresql://ayan:O72akEpA1Z3IEYwiQzS8wkdRel00JjAv@dpg-d288ptp5pdvs738ag9q0-a.oregon-postgres.render.com/eventmate_1438',
+    ssl: { rejectUnauthorized: false }
 });
 
 
