145c43eb615922d3af35edc1dbdd2a03eb2b7412
Jarv1s-stack <ayanabdimutalip@gmail.com>
2025-08-04 20:45:50 +0500

    -delete function in the file userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,20 +36,20 @@ exports.getOtherProfile = async (req, res) => {
 }
 
 
-exports.getProfile = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const result = await pool.query(
-      'SELECT id, username, email, points, avatar, created_at FROM users WHERE id = $1',
-      [userId]
-    );
-    if (!result.rows.length) return res.status(404).json({ message: 'Пользователь не найден' });
-    res.json(result.rows[0]);
-  } catch (e) {
-    console.error("Get user profile error:", e);
-    res.status(500).json({ message: 'Ошибка сервера' });
-  }
-};
+// exports.getProfile = async (req, res) => {
+//   try {
+//     const userId = req.params.id;
+//     const result = await pool.query(
+//       'SELECT id, username, email, points, avatar, created_at FROM users WHERE id = $1',
+//       [userId]
+//     );
+//     if (!result.rows.length) return res.status(404).json({ message: 'Пользователь не найден' });
+//     res.json(result.rows[0]);
+//   } catch (e) {
+//     console.error("Get user profile error:", e);
+//     res.status(500).json({ message: 'Ошибка сервера' });
+//   }
+// };
 
 
 exports.getPoints = async (req, res) => {
